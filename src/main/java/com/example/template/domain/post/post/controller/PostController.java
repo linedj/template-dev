21983281f861ba9f0b6b7966ef9d727d3ca1e83c
linedj <eunjun998@gmail.com>
2025-01-23 14:37:54 +0900

    get/post

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -1,9 +1,7 @@
 package com.example.template.domain.post.post.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/posts")
@@ -13,7 +11,7 @@ public class PostController {
     @ResponseBody
     public String showWrite() {
         return """
-                <form action="/posts/doWrite">
+                <form action="/posts/doWrite" method="post">
                     <input type="text" name="title" placeholder="제목">
                     <textarea name="content"></textarea>
                     <input type="submit" value="등록" />
@@ -21,7 +19,7 @@ public class PostController {
                 """;
     }
 
-    @GetMapping("/doWrite")
+    @PostMapping("/doWrite")
     @ResponseBody
     public String doWrite(String title, String content) {
         return """
