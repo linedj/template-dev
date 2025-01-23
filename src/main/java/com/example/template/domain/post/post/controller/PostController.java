2fa1060661f9b42c6049f064dad08fa0a8ba0421
linedj <eunjun998@gmail.com>
2025-01-23 14:05:14 +0900

    hello

---
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -0,0 +1,16 @@
+package com.example.template.domain.post.post.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "<h1>hello</h1>";
+    }
+
+}
