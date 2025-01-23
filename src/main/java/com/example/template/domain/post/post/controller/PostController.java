0a1113b90e9519dff74277d988a0a68f12e0f839
linedj <eunjun998@gmail.com>
2025-01-23 14:17:03 +0900

    show wirte

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -2,15 +2,23 @@ package com.example.template.domain.post.post.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/posts")
 public class PostController {
 
-    @GetMapping("/")
+    @GetMapping("/write")
     @ResponseBody
-    public String hello() {
-        return "<h1>hello</h1>";
+    public String showWrite() {
+        return """
+                <form action="/doWrite">
+                    <input type="text" name="title" placeholder="제목">
+                    <textarea name="content"></textarea>
+                    <input type="submit" value="등록" />
+                </form>
+                """;
     }
 
 }
