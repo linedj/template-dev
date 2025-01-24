813cb6f1060ef21face3c3cbd585279b1f6ed65a
linedj <eunjun998@gmail.com>
2025-01-24 14:23:17 +0900

    fin

--- src/main/java/com/example/template/domain/post/post/controller/PostController.java
+++ src/main/java/com/example/template/domain/post/post/controller/PostController.java
@@ -1,10 +1,12 @@
 package com.example.template.domain.post.post.controller;
 
 import com.example.template.domain.post.post.entity.Post;
+import com.example.template.domain.post.post.service.PostService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,39 +16,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/posts")
+@RequiredArgsConstructor
 public class PostController {
 
-    private List<Post> posts = new ArrayList();
-    private long lastId = 3L;
-
-    public PostController() {
-        Post p1 = Post.builder()
-                .id(1L)
-                .title("title1")
-                .content("content1")
-                .build();
-
-        Post p2 = Post.builder()
-                .id(2L)
-                .title("title2")
-                .content("content2")
-                .build();
-
-        Post p3 = Post.builder()
-                .id(3L)
-                .title("title3")
-                .content("content2")
-                .build();
-
-        posts.add(p1);
-        posts.add(p2);
-        posts.add(p3);
-    }
+    private final PostService postService;
 
     @GetMapping("/write")
     public String showWrite(WriteForm form, BindingResult bindingResult) {
@@ -73,13 +50,7 @@ public class PostController {
             return "domain/post/post/write";
         }
 
-        Post post = Post.builder()
-                .id(++lastId)
-                .title(form.getTitle())
-                .content(form.getContent())
-                .build();
-
-        posts.add(post);
+        postService.write(form.getTitle(), form.getContent());
 
         return "redirect:/posts"; //리다이렉트
     }
@@ -87,20 +58,18 @@ public class PostController {
     @GetMapping
     private String showList(Model model) {
 
+        List<Post> posts = postService.getItems();
         model.addAttribute("posts", posts);
+
         return "domain/post/post/list";
     }
 
     @GetMapping("/detail/{id}")
     private String detail(@PathVariable long id, Model model) {
 
-        Post post = posts.stream()
-                .filter(p -> p.getId() == id)
-                .findFirst()
-                .get();
+        Post post = postService.getItem(id).get();
 
         model.addAttribute("post", post);
-
         return "domain/post/post/detail";
     }
 
